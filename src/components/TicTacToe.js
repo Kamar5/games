4c24be2ae8b36bd5bfaa85eb1f3b71adbad398f0
Kamar5 <qfk51994@gmail.com>
2018-12-17 16:54:18 -0500

    refacter the code to reuse and remove duplicate code

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import autoBind from 'react-autobind';
-import { Well, Button } from 'react-bootstrap';
+import { Well, Button, Modal } from 'react-bootstrap';
 
 
 export default class TicTacToe extends React.Component {
@@ -85,7 +85,20 @@ export default class TicTacToe extends React.Component {
 
       return true;
     }else{
-      return false
+      const hasEmptyField = this.state.tictactoeSet.find(function(object) {
+        return object.value === ''
+      })
+      if(hasEmptyField){
+        return false
+      }else{
+        this.setState({
+          player1: { ...this.state.player1, score: this.state.player1.score + 0.5},
+          player2: { ...this.state.player2, score: this.state.player2.score + 0.5},
+          winner: 'Draw',
+          gameOver: true
+        })
+      }
+      
     }
   }
   onUserClick(position) {
@@ -95,30 +108,23 @@ export default class TicTacToe extends React.Component {
       alert('you already entered');
     }else{
       let currentStateTictactoe = this.state.tictactoeSet;
-      const searhForPosition = this.state.tictactoeSet.map(function(x) {return x.id; }).indexOf(position.id);
+      let player1 = this.state.player1;
+      let player2 = this.state.player2;
+      const searchForPosition = this.state.tictactoeSet.map(function(x) {return x.id; }).indexOf(position.id);
       if(this.state.player1.turn){
-        currentStateTictactoe[searhForPosition].value = this.state.player1.type;
-        let player1 = this.state.player1;
-        let player2 = this.state.player2;
+        currentStateTictactoe[searchForPosition].value = player1.type;
         player1.turn = false;
         player2.turn = true;
-        this.setState({
-          player1: player1,
-          player2: player2,
-          tictactoeSet: [...currentStateTictactoe]
-        }, () => this.winCombination());
       }else{
-        currentStateTictactoe[searhForPosition].value = this.state.player2.type;
-        let player1 = this.state.player1;
-        let player2 = this.state.player2;
+        currentStateTictactoe[searchForPosition].value = player2.type;
         player1.turn = true;
         player2.turn = false;
-        this.setState({
-          player1: player1,
-          player2: player2,
-          tictactoeSet: [...currentStateTictactoe]
-        }, () => this.winCombination());
       }     
+      this.setState({
+        player1: player1,
+        player2: player2,
+        tictactoeSet: [...currentStateTictactoe]
+      }, () => this.winCombination());
     }
   }
 
@@ -169,44 +175,76 @@ export default class TicTacToe extends React.Component {
       gameOver: false
     })
   }
+  resetGame(){
+    let tempTictactoeSet = this.state.tictactoeSet;
+
+    tempTictactoeSet.map(function(x) { 
+      x.value = ''; 
+      return x
+    });
+    this.setState({
+      player1: {
+        score: 0,
+        turn: true,
+        type: 'X',
+        initTurn: true
+      },
+      player2: {
+        score: 0,
+        turn: false,
+        type: 'O',
+        initTurn: false
+      },
+      winner: '',
+      gameOver: false,
+      tictactoeSet: tempTictactoeSet
+    })
+  }
+
+  renderModel(){
+    return(<Modal show={this.state.gameOver} >
+              <Modal.Header>
+                <Modal.Title>{this.state.winner} this round</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h1 style={{textAlign: 'center'}}>Player 1 vs Player 2</h1>
+                <h2 style={{textAlign: 'center'}}>{this.state.player1.score} - {this.state.player2.score}</h2>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.resetBoard}>Play Again</Button>
+              </Modal.Footer>
+            </Modal>)
+  }
+  renderWellField(fieldLocation, styleOfField){
+    return (<Well style={{...styleOfField, cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0',textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(fieldLocation))}>
+              {this.findArrayElementById(fieldLocation).value}
+            </Well>)
+  }
+
+  renderButtonField(onClickFuc, buttonName) {
+    return <Button bsSize="small" style={{  marginTop: '10px', marginLeft: '5px'}} bsStyle="danger" onClick={onClickFuc}>{buttonName}</Button>
+  }
+
   render() {
     return (
       <div className="TICTACTOE" style={{position: 'absolute', left: '50%', transform: 'translate(-50%, 0)'}}>
+        {this.renderModel()}
         <div style={{marginTop: '17px', marginLeft: '74px'}}><span style={{color: '#00ff51', borderBottom: this.state.player1.turn ? '5px solid #37e937' : ''}}> Player 1(X)</span> <span style={{color: 'red'}}>vs</span> <span style={{color: '#00ff51',borderBottom: this.state.player2.turn ? '5px solid #37e937' : ''}}>Player 2(O)</span></div>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '17px', float: 'left', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(11))}>
-          {this.findArrayElementById(11).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '17px', marginLeft: '1px', float: 'left', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(12))}>
-          {this.findArrayElementById(12).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '17px', marginLeft: '202px', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(13))}>
-          {this.findArrayElementById(13).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', float: 'left', textAlign: 'center', fontSize: '40px' }} onClick={this.onUserClick.bind(this,this.findArrayElementById(21))}>
-          {this.findArrayElementById(21).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', marginLeft: '1px', float: 'left', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(22))}>
-          {this.findArrayElementById(22).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', marginLeft: '202px', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(23))}>
-          {this.findArrayElementById(23).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', float: 'left', textAlign: 'center', fontSize: '40px' }} onClick={this.onUserClick.bind(this,this.findArrayElementById(31))}>
-          {this.findArrayElementById(31).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', marginLeft: '1px', float: 'left', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(32))}>
-          {this.findArrayElementById(32).value}
-        </Well>
-        <Well style={{cursor: 'pointer',width:'100px', height:'100px', marginBottom: '0', marginTop: '1px', marginLeft: '202px', textAlign: 'center', fontSize: '40px'}} onClick={this.onUserClick.bind(this,this.findArrayElementById(33))}>
-          {this.findArrayElementById(33).value}
-        </Well>
-        <div style={{color: '#00ff51',marginTop: '10px', float: 'left'}}>
-          Player 1: {this.state.player1.score} 
+        {this.renderWellField(11,{ marginTop: '17px', float: 'left'} )}
+        {this.renderWellField(12,{ marginTop: '17px', marginLeft: '1px', float: 'left'} )}
+        {this.renderWellField(13,{ marginTop: '17px', marginLeft: '202px'} )}
+        {this.renderWellField(21,{ marginTop: '1px', float: 'left' } )}
+        {this.renderWellField(22,{ marginTop: '1px', marginLeft: '1px', float: 'left'} )}
+        {this.renderWellField(23,{ marginTop: '1px', marginLeft: '202px'} )}
+        {this.renderWellField(31,{ marginTop: '1px', float: 'left' } )}
+        {this.renderWellField(32,{ marginTop: '1px', marginLeft: '1px', float: 'left'} )}
+        {this.renderWellField(33,{ marginTop: '1px', marginLeft: '202px'} )}
+        <div style={{color: '#00ff51',marginTop: '2px', float: 'left'}}>
+          Player 1: {this.state.player1.score} <br />
+          Player 2: {this.state.player2.score} 
         </div>
-        <div style={{color: '#00ff51', marginTop: '10px', marginLeft: '17px', float: 'left'}}>
-          Player 2: {this.state.player2.score}           
-        </div>
-        <Button style={{  marginTop: '6px', marginLeft: '17px'}} bsStyle="danger" onClick={this.resetBoard}>Reset the board</Button>
+        {this.renderButtonField(this.resetGame, 'Restart the game')}
+        {this.renderButtonField(this.resetBoard, 'Reset the board')}
       </div>
     );
   }
--- src/components/TicTacToe.test.js
+++ src/components/TicTacToe.test.js
@@ -6,13 +6,21 @@ import { Well, Button } from 'react-bootstrap';
 it('should render with well and button compoents', () => {
   const wrapper = shallow(<TicTacToe />);
   expect(wrapper.find(Well).length).toEqual(9);
-  expect(wrapper.find(Button).length).toEqual(1);
+  expect(wrapper.find(Button).length).toEqual(3);
 });
 
-it('should reset to defualt values when click reset button and make the Player2 turn', () => {
+it('should reset to defualt values when click restart button', () => {
     const wrapper = mount(<TicTacToe />);
 
-    wrapper.find(Button).simulate('click');
-    expect(wrapper.state().player1.turn).toEqual(false);
-    expect(wrapper.state().player2.turn).toEqual(true);
+    wrapper.find(Button).at(0).simulate('click');
+    expect(wrapper.state().player1.turn).toEqual(true);
+    expect(wrapper.state().player2.turn).toEqual(false);
+});
+
+it('should reset to board values when click reset board button', () => {
+  const wrapper = mount(<TicTacToe />);
+
+  wrapper.find(Button).at(1).simulate('click');
+  expect(wrapper.state().player1.turn).toEqual(false);
+  expect(wrapper.state().player2.turn).toEqual(true);
 });
